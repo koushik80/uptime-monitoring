91d32341f76cb59e2e21db27bc7908e49f26c17b
koushik <koushik.dey@live.com>
2022-10-19 14:58:16 +0300

    request body

--- index.js
+++ index.js
@@ -8,13 +8,15 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-    port: 3000
+    port: 3000,
 };
 
 // create server
@@ -27,9 +29,28 @@ app.createServer = () => {
 
 // handle request & response
 app.handleReqRes = (req, res) => {
-    // response handle
-    res.send('Hello API');
+     // request handling
+     // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
 
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Hello World!');
+     });
 };
 
 // start the server
