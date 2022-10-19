860f56a57fb4f95295c641a29efaac63178a52c2
koushik <koushik.dey@live.com>
2022-10-19 22:51:40 +0300

    environmen setup

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -40,9 +40,20 @@ handler.handleReqRes = (req, res) => {
 
     const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
 
-    chosenHandler(requestProperties, (statusCode, payload) => {
-        statusCode = typeof (statusCode) === 'number' ? statusCode : 500;
-        payload = typeof (payload) === 'object' ? payload : {};
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+
+        chosenHandler(requestProperties, (statusCode, payload) => {
+            statusCode =
+                typeof (statusCode) === 'number'
+                ? statusCode : 500;
+            payload =
+                typeof (payload) === 'object'
+                ? payload : {};
 
         const payloadString = JSON.stringify(payload);
 
@@ -50,14 +61,6 @@ handler.handleReqRes = (req, res) => {
         res.writeHead(statusCode);
         res.end(payloadString);
     });
-
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer);
-    });
-
-    req.on('end', () => {
-        realData += decoder.end();
-        console.log(realData);
         // response handle
         res.end('Hello World!');
     });
