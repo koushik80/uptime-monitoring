4b4797bec70df726acf95922a6109975aec04592
koushik <koushik.dey@live.com>
2022-10-21 02:25:34 +0300

    server & worker lib

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
 /*
- * Title: Uptime Monitoring Application
- * Description: A RESTFul API to monitor up or down time of user defined links
+ * Title:  Project Initial file
+ * Description: Initial file to start the node server and workers
  * Author: Koushik
- * Date: 19.10.2022
+ * Date: 19.10.2022 updated on 20.10.2022
  *
  */
 
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-//const environments = require('./helpers/environments');
-//const data = require('./lib/data');
+
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
-
-// configuration
-app.config = {
-    port: 3000,
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
+app.init();
 
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
-    });
-};
-
-// handle request & response
-app.handleReqRes = handleReqRes;
-
-// start the server
-app.createServer();
+// export the app
+module.exports = app;
